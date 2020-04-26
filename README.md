db7350ded9506b3fd88be3462b57b8103c919b76
Viktor Barsk <git@viktorbarsk.se>
2020-04-26 06:51:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Raytracer
+
+Raytracer based on (Raytracing in a Weekend)[https://raytracing.github.io/books/RayTracingInOneWeekend.html].
+
+## Usage
+
+    go run raytracer.go ; convert out/hello_world.ppm out/hello_world.png
---
+++ raytracer.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	// "bufio"
+	"fmt"
+	// "io/ioutil"
+	"log"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	filename := "out/hello_world.ppm"
+	f, err := os.Create(filename)
+	check(err)
+	defer f.Close()
+
+	imageWidth := 200
+	imageHeight := 100
+
+	fmt.Fprintf(f, "P3\n%d\n%d\n255\n", imageWidth, imageHeight)
+
+	for j := imageHeight - 1; j >= 0; j-- {
+		log.Printf("Scanlines remaining: %d", j)
+		for i := 0; i < imageWidth; i++ {
+			r := float64(i) / float64(imageWidth)
+			g := float64(j) / float64(imageHeight)
+			b := 0.2
+
+			ir := int16(255.999 * r)
+			ig := int16(255.999 * g)
+			ib := int16(255.999 * b)
+
+			fmt.Fprintf(f, "%d %d %d\n", ir, ig, ib)
+			check(err)
+		}
+	}
+	log.Printf("Done writing %s", filename)
+}
